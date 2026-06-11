41bfdf887dd8f93e49360d0636f0406547c9c8eb
tatuo-an <tatuo.an@gmail.com>
2026-06-11 21:39:12 +0900

    a

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+      },
+      {
+        protocol: "https",
+        hostname: "*.microcms-assets.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
